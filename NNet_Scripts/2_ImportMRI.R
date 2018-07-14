346b4eba85f6744cb4c1d772e9fb3c1c70e3698e
Melinda Mortimer <melinda.mortimer1@gmail.com>
2018-07-14 23:16:37 +1000

    View and import .nii

--- NNet_Scripts/2_ImportMRI.R
+++ NNet_Scripts/2_ImportMRI.R
@@ -1,1 +1,25 @@
 library(AnalyzeFMRI)
+hdr <- f.read.header("../tenSubjects/0046_t1_MAS_w1.nii")
+hdr$dim
+# Dimensions of image: 
+# 3D
+# x: 256 (slice width)
+# y: 190 (slice height)
+# z: 245 (volume depth)
+
+hdr$datatype
+# Datatype 4: Signed short
+
+hdr$pixdim
+# voxel width: 1mm
+# voxel height: 1mm
+# slice thickness: 1mm
+# timeslice: 1
+
+A <- f.read.nifti.volume("../tenSubjects/0046_t1_MAS_w1.nii")
+dim(A)
+image(A[138,,,])
+
+B <- f.read.nifti.slice("../tenSubjects/0046_t1_MAS_w1.nii",
+                        slice = 100, tpt = 1)
+image(B)
