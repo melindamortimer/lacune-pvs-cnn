e6b913bd5146724b25889e2116e3ce8ed9300cd9
melindamortimer <melinda.mortimer1@gmail.com>
2018-08-07 23:25:13 +1000

    Data structure planning

--- NNet_Scripts/2_ImportMRI.R
+++ NNet_Scripts/2_ImportMRI.R
@@ -23,3 +23,40 @@ image(A[,,150,], col = grey.colors(100))
 B <- f.read.nifti.slice("0046_t1_MAS_w1.nii",
                         slice = 100, tpt = 1)
 image(B, col = grey.colors(100))
+
+
+
+# Data Structure ----------------------------------------------------------
+
+# Original image data format is as a nifti file:
+# - dimensions?
+
+# lacune data is currently in an excel sheet. Lacunes will be manually identified within either:
+# - excel spreadsheet that lists scan id & x/y/z coords of lacunes. Note all involved pixels
+# - a mask of equal dimension that gives value 1 to regions of lacune, 0 otherwise
+
+# First part of the model requires:
+# - 51x51 axial patches
+# - 0/1 lacune flag
+
+# Second part of the model requires:
+# - 32x32x5 blocks
+# - 64x64x5 blocks, reshaped to 32x32x5
+# - 128x128x5 blocks, reshaped to 32x32x5
+# - 0/1 lacune flag
+# - Vector of 7 location features:
+  # - x/y/z coords
+  # - distance to left ventricle
+  # - distance to right ventricle
+  # - distance to cerebral cortex
+  # - distance to midsaggital brain surface
+
+# If lacune data logged via excel, create a data matrix the same dimensions as the scan, with all responses first set to 0. Then go back and iterate through excel sheet, and convert identified pixel responses to 1.
+
+# If lacunes identified by colouring in masks, then the data will be on a separate nifti file of same dimensions as the original image. Import and change to matrix via AnalyzeFMRI.
+
+# Keep each sample together.
+# For the first model, each x/y/z is a sample of two variables: the 2D image and the flag.
+# For the second model, each x/y/z is a sample of 11 variables: the three 3D images, flag and 7 location features.
+
+# ALTERNATIVELY store for both models by just storing for the second, then retrieving variables for the first by taking the middle slice of the 32x32x5 block.
